84d92e45b68b386cabe0cfbe709b502f8da90cdb
Simon Nestler <simon.nestler@thi.de>
2024-06-27 17:14:34 +0200

    final commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Datum d = new Datum(1, 2, 2021);
+        System.out.println(d.getTag());
+        System.out.println(d.getMonat());
+        System.out.println(d.getJahr());
     }
 }
